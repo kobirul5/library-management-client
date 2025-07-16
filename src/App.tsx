bcb269091af6059e592e49738838c8602c75c4a3
Md. Kobirul Islam <kobirul05j@gmail.com>
2025-07-16 10:01:17 +0600

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <p className="read-the-docs">
+        Welcome to library management 
+      </p>
+    </>
+  )
+}
+
+export default App
